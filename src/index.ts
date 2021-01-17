94f893b34f9aa2fab99bddf3913c710e8c662b0c
kaypee <resaba.kaypee@gmail.com>
2021-01-17 15:16:57 +0700

    initialize the application

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello world");
+import { HTMLMediaManView } from "./view/MediaManView";
+import { bookService, movieService } from "./controller/MediaService";
+import {
+  MediaManControllerImpl,
+  MediaManController,
+} from "./controller/MediaManController";
+
+const view: HTMLMediaManView = new HTMLMediaManView();
+
+const mediaManController = new MediaManControllerImpl(
+  view,
+  bookService,
+  movieService
+);
+
+interface CustomWindow extends Window {
+  mediaManController?: MediaManController;
+}
+
+const customWindow: CustomWindow = window;
+customWindow.mediaManController = mediaManController;
+
+console.log("MediaMan ready!", customWindow.mediaManController);
